fd00989fbd642a4273c7ed82addd04e8e0d8766b
Stanislav Ussov <stanislav.ussov@livesport.eu>
2025-02-15 20:49:47 +0100

    capacitor deps

---
+++ packages/client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.translator.client',
+  appName: 'Translator Client',
+  webDir: 'dist'
+};
+
+export default config;
